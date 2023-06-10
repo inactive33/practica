b3e08688ea988ccdfec033af8507e0917ec7b9b2
inactive33 <325vi7001@gmail.com>
2023-06-10 16:18:44 +0300

    Commit

---
+++ pract02/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace pract02
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick_1 = new System.Windows.Forms.Button();
+            this.btnClick_2 = new System.Windows.Forms.Button();
+            this.btnClick_3 = new System.Windows.Forms.Button();
+            this.btnClick_4 = new System.Windows.Forms.Button();
+            this.btnClick_5 = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick_1
+            // 
+            this.btnClick_1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnClick_1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick_1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnClick_1.Location = new System.Drawing.Point(12, 51);
+            this.btnClick_1.Name = "btnClick_1";
+            this.btnClick_1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClick_1.Size = new System.Drawing.Size(93, 73);
+            this.btnClick_1.TabIndex = 0;
+            this.btnClick_1.Text = "1";
+            this.btnClick_1.UseVisualStyleBackColor = false;
+            // 
+            // btnClick_2
+            // 
+            this.btnClick_2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnClick_2.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick_2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnClick_2.Location = new System.Drawing.Point(111, 51);
+            this.btnClick_2.Name = "btnClick_2";
+            this.btnClick_2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClick_2.Size = new System.Drawing.Size(93, 73);
+            this.btnClick_2.TabIndex = 1;
+            this.btnClick_2.Text = "2";
+            this.btnClick_2.UseVisualStyleBackColor = false;
+            // 
+            // btnClick_3
+            // 
+            this.btnClick_3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnClick_3.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick_3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnClick_3.Location = new System.Drawing.Point(210, 51);
+            this.btnClick_3.Name = "btnClick_3";
+            this.btnClick_3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClick_3.Size = new System.Drawing.Size(93, 73);
+            this.btnClick_3.TabIndex = 2;
+            this.btnClick_3.Text = "3";
+            this.btnClick_3.UseVisualStyleBackColor = false;
+            // 
+            // btnClick_4
+            // 
+            this.btnClick_4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnClick_4.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick_4.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnClick_4.Location = new System.Drawing.Point(309, 51);
+            this.btnClick_4.Name = "btnClick_4";
+            this.btnClick_4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClick_4.Size = new System.Drawing.Size(93, 73);
+            this.btnClick_4.TabIndex = 3;
+            this.btnClick_4.Text = "4";
+            this.btnClick_4.UseVisualStyleBackColor = false;
+            // 
+            // btnClick_5
+            // 
+            this.btnClick_5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnClick_5.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick_5.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnClick_5.Location = new System.Drawing.Point(408, 51);
+            this.btnClick_5.Name = "btnClick_5";
+            this.btnClick_5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClick_5.Size = new System.Drawing.Size(93, 73);
+            this.btnClick_5.TabIndex = 4;
+            this.btnClick_5.Text = "5";
+            this.btnClick_5.UseVisualStyleBackColor = false;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(207, 24);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(114, 13);
+            this.lblDescription.TabIndex = 5;
+            this.lblDescription.Text = "Очистите все кнопки";
+            // 
+            // startbtn
+            // 
+            this.startbtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startbtn.Font = new System.Drawing.Font("Sitka Small", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbtn.Location = new System.Drawing.Point(12, 12);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(488, 112);
+            this.startbtn.TabIndex = 6;
+            this.startbtn.Text = "Начать";
+            this.startbtn.UseVisualStyleBackColor = false;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click_1);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 136);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.btnClick_5);
+            this.Controls.Add(this.btnClick_4);
+            this.Controls.Add(this.btnClick_3);
+            this.Controls.Add(this.btnClick_2);
+            this.Controls.Add(this.btnClick_1);
+            this.Controls.Add(this.startbtn);
+            this.Name = "GameForm";
+            this.Text = "Игра";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick_1;
+        private System.Windows.Forms.Button btnClick_2;
+        private System.Windows.Forms.Button btnClick_3;
+        private System.Windows.Forms.Button btnClick_4;
+        private System.Windows.Forms.Button btnClick_5;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button startbtn;
+    }
+}
+
---
+++ pract02/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace pract02
+{
+    /// <summary>
+    /// Разделяемый производный класс GameForm от Form.
+    /// Отвечает за создание формы и инициализацию компонентов
+    /// В нём реализована игра
+    /// </summary>
+    public partial class GameForm : Form
+    {
+        List<Button> buttons; // Создаём коллекцию для кнопок
+        /// <summary>
+        /// Конструктор GameForm() инициализирует объекты на форму
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent(); // Инициализируем компоненты
+            Button startbtn = new Button(); // Инициализиуем объект для стартовой кнопки
+            startbtn.Click += startbtn_Click_1; // Обрабатываем нажатие на стартовую кнопку вместе с методом
+            buttons = new List<Button>(); // Инициализируем объект в коллекцию
+            // В объекте коллекции создаём список из кнопок размерностью 5
+            buttons.AddRange(new Button[5] { btnClick_1, btnClick_2, btnClick_3, btnClick_4, btnClick_5 }); 
+            btnClick_1.Click += btn_Click;
+            btnClick_2.Click += btn_Click;
+            btnClick_3.Click += btn_Click;
+            btnClick_4.Click += btn_Click;
+            btnClick_5.Click += btn_Click;
+
+        }
+        /// <summary>
+        /// Метод Restart() предлагает перезапустить игру
+        /// соотвественно инициализируя заного компоненты
+        /// </summary>
+        public void Restart()
+        {
+           foreach (Button btn in buttons) {
+                if (btn.Visible) return;
+            }
+                // Диалог пользователя
+                DialogResult result = MessageBox.Show(
+                    "Начать заново?", "Сообщение", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    InitializeComponent();
+                }
+                else this.Close();
+        }
+        /// <summary>
+        /// Метод btn_Click отвечает за обработку нажатия основных игровых кнопок
+        /// </summary>
+        /// <param name="sender", объект, вызвавший событие>
+        /// <param name="e", экземпляр EventArgs включения>
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            Button btn = buttons[random.Next(buttons.Count)];
+            btn.Visible = !btn.Visible;
+            Restart();
+        }
+
+        /// <summary>
+        /// Метод startbtn_Click_1 обработка нажатия для старовой кнопки
+        /// </summary>
+        /// <param name="sender", объект, вызвавший событие>
+        /// <param name="e", экземпляр EventArgs включения>
+        private void startbtn_Click_1(object sender, EventArgs e)
+        {
+            // Скрываем стартовую кнопку
+            startbtn.Visible = false;
+        }
+    }
+}
