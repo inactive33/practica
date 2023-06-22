fabe5ffcbf9889fd6473ffada50487fb5477919d
WuserVR <strannyesajty@gmail.com>
2023-06-22 11:54:17 +0300

    practica 5

---
+++ pract02_5/SolutionForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace pract02_5
+{
+    partial class SolutionForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBoxData = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstBoxData
+            // 
+            this.lstBoxData.FormattingEnabled = true;
+            this.lstBoxData.Location = new System.Drawing.Point(28, 32);
+            this.lstBoxData.Name = "lstBoxData";
+            this.lstBoxData.Size = new System.Drawing.Size(318, 160);
+            this.lstBoxData.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 216);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(95, 40);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Пуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(144, 230);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 13);
+            this.lblTotal.TabIndex = 2;
+            // 
+            // SolutionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 286);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstBoxData);
+            this.Name = "SolutionForm";
+            this.Text = "SolutionForm";
+            this.Load += new System.EventHandler(this.SolutionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBoxData;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ pract02_5/SolutionForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace pract02_5
+{
+    /// <summary>
+    /// Класс SolutionForm
+    /// Отвечает за инициализацию компонентов и их обработку
+    /// </summary>
+    public partial class SolutionForm : Form
+    {
+        /// <summary>
+        /// Конструктор инициализирует компоненты на форму
+        /// </summary>
+        public SolutionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SolutionForm_Load(object sender, EventArgs e)
+        {
+            lstBoxData.Items.Add("We will rest and you drive.");
+            lstBoxData.Items.Add("It 2 sentenses.");
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки.
+        /// Преобразуем текст из listbox
+        /// В текст с прописными буквами у каждого слова
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Создаём экземпляр списка текста
+            List<string> text = new List<string>();
+
+            // Перебираем все item и представляем как текст
+            // Добавляем это в коллекцию текста
+            foreach(var item in lstBoxData.Items)
+            {
+                text.Add(item.ToString());
+            }
+            // Создаём ещё экземпляр коллекции для модифицирования текста
+            List<string> modifiedText = new List<string>();
+            // Перебираем предложения в тексте
+            foreach (var sentence in text)
+            {   // Создаём массив слов и разделяем предложение на подстроки пробелами
+                string[] words = sentence.Split(' '); 
+                string modifiedSentences = ""; // Создаём пустую строку, в которой будем хранить итоговое предложение
+                foreach (var word in words) // Перебираем слова в предложении
+                {
+                    // Создаём модифицированное слово
+                    // В каждом слове увеличиваем 1 букву,
+                    // (берём первую букву (индекс - 0) и прибавляем оставшуюся подстроку) 
+                    string modifiedWord = char.ToUpper(word[0]) + word.Substring(1);
+                    // Прибавляем каждое модифицированное слово и разделяем пробелами
+                    modifiedSentences += modifiedWord + " ";
+                }
+                // Добавляем в текст наши предложения, удаляя начальные и конечнные символы пробелов
+            modifiedText.Add(modifiedSentences.Trim()); 
+            }
+            // Записываем в label наш текст
+            lblTotal.Text = string.Join(Environment.NewLine, modifiedText);
+        }
+    }
+}
